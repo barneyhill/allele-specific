46e166c9267e4da51c84400f8b69a70fa2bacce9
Barney Hill <dri461@cluster1.hpc.in.bmrc.ox.ac.uk>
2025-09-01 18:33:50 +0100

    download script

---
+++ download_1kG.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p 1kGP_vcfs
+cd 1kGP_vcfs
+
+for chr in {1..22}; do
+    wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chr${chr}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz
+    wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chr${chr}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz.tbi
+done
